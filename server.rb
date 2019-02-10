a166fbbbcd9d87b6aff7d3b075b2e5208182d17b
Kyle Fazzari <kyrofa@ubuntu.com>
2019-02-09 21:57:23 -0800

    server: make github private key optional
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

--- server.rb
+++ server.rb
@@ -14,7 +14,7 @@ set :bind, '0.0.0.0'
 class GHAapp < Sinatra::Application
 	# Converts the newlines. Expects that the private key has been set as an
 	# environment variable in PEM format.
-	PRIVATE_KEY = OpenSSL::PKey::RSA.new(ENV['GITHUB_PRIVATE_KEY'].gsub('\n', "\n"))
+	PRIVATE_KEY = OpenSSL::PKey::RSA.new(ENV['GITHUB_PRIVATE_KEY'].gsub('\n', "\n")) if ENV.include? 'GITHUB_PRIVATE_KEY'
 
 	# Your registered app must have a secret set. The secret is used to verify
 	# that webhooks are sent by GitHub.
