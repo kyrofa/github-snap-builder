12861a2efeeda265124d4d1fa243e1af1b22c733
Kyle Fazzari <kyrofa@ubuntu.com>
2019-02-17 18:22:45 -0500

    docker: use pre-built image instead of building
    Unable to use unsquashfs in an unprivileged docker container in an
    unprivileged LXD container.
    
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

--- lib/github_snap_builder/builder_implementations/docker.rb
+++ lib/github_snap_builder/builder_implementations/docker.rb
@@ -42,17 +42,19 @@ module GithubSnapBuilder
 		private
 
 		def run(command, options)
-			# Grab (or build) the image for this base
+			# Grab the image for this base
 			image = get_image(@base)
 			options = {'Image' => image.id, 'Cmd' => command}.merge(options)
 
 			begin
-				# Create a new container, mounting in the current working directory
-				# (which is the build directory), and run snapcraft on it.
+				# Create a new container
 				container = Docker::Container.create options
 
+				# Give called the change to mess with container before running
+				# anything
 				yield container if block_given?
 
+				# Fire up the container and stream its logs
 				container.tap(&:start).attach do |stream, chunk|
 					puts "#{stream}: #{chunk}"
 				end
@@ -67,40 +69,8 @@ module GithubSnapBuilder
 			end
 		end
 
-		def dockerdir
-			gem_root = File.dirname(File.dirname(File.dirname(__dir__)))
-			File.join(gem_root, 'docker')
-		end
-
-		def dockerfile_name(base)
-			"Dockerfile.#{base}"
-		end
-
 		def get_image(base)
-			repo = 'github-snap-builder'
-			tag = base
-
-			begin
-				image = Docker::Image.get("#{repo}:#{tag}")
-				created = DateTime.parse(image.info["Created"])
-
-				# The image is only considered valid if it's under a day old
-				if DateTime.now < created.next_day
-					return image
-				else
-					image.remove(force: true)
-				end
-			rescue Docker::Error::NotFoundError
-				# Do nothing
-			end
-
-			# If we got here, then either no image exists, or the one that did
-			# was too old and was pruned. Either way, create a new one.
-			image = Docker::Image.build_from_dir dockerdir, {
-				dockerfile: dockerfile_name(base)
-			}
-			image.tag(repo: repo, tag: tag)
-			return image
+			Docker::Image.create('fromImage' => "github-snap-builder:#{base}")
 		end
 	end
 end
--- tests/builder_implementations/docker_test.rb
+++ tests/builder_implementations/docker_test.rb
@@ -6,11 +6,6 @@ module GithubSnapBuilder
 	class DockerBuilderTest < SnapBuilderBaseTest
 		def setup
 			@mock_image = mock('image')
-			@mock_image.stubs(:info).returns({'Created' => DateTime.now.to_s})
-
-			@mock_old_image = mock('image')
-			@mock_old_image.stubs(:info).returns({'Created' => DateTime.now.prev_day.prev_day.to_s})
-
 			@mock_container = mock('container')
 			Docker.stubs(:validate_version!)
 		end
@@ -23,59 +18,18 @@ module GithubSnapBuilder
 			end
 		end
 
-		def test_build_no_existing_image
-			Docker::Image.expects(:get).with("github-snap-builder:test-base").raises(Docker::Error::NotFoundError)
-
-			# Now expect a new image to be built and tagged
-			assert_image_created
-			assert_successful_build
-		end
-
-		def test_build_old_image
-			# Expect the old image to be fetched and then deleted
-			Docker::Image.expects(:get).with("github-snap-builder:test-base").returns(@mock_old_image)
-			@mock_old_image.expects(:remove).with(force: true)
-
-			# Now expect a new image to be built and tagged
-			assert_image_created
-			assert_successful_build
-		end
-
-		def test_build_existing_image
-			Docker::Image.expects(:get).with("github-snap-builder:test-base").returns(@mock_image)
-			assert_successful_build
+		def test_build_success
+			assert_build 0
 		end
 
 		def test_build_failure
-			Docker::Image.expects(:get).with("github-snap-builder:test-base").returns(@mock_image)
-
-			# Expect container based on image to be fired up
-			@mock_image.expects(:id).returns('1234')
-			Docker::Container.expects(:create).with(
-				'Cmd' => ['sh', '-c', "apt update -qq && snapcraft --destructive-mode"],
-				'Image' => '1234',
-				'Env' => ['SNAPCRAFT_MANAGED_HOST=yes'],
-				'WorkingDir' => '/snapcraft',
-				'HostConfig' => {
-					'Binds' => ["test-project-dir:/snapcraft"],
-					'AutoRemove' => true,
-				}
-			).returns(@mock_container)
-			@mock_container.expects(:start)
-			@mock_container.expects(:attach)
-
-			# Mock the failure, but ensure container is still deleted
-			@mock_container.expects(:wait).returns({'StatusCode' => 1})
-			@mock_container.expects(:delete).with(force: true)
-
-			builder = DockerBuilder.new('test-base')
 			assert_raises DockerRunError do
-				builder.build('test-project-dir')
+				assert_build 1
 			end
 		end
 
 		def test_release
-			Docker::Image.expects(:get).with("github-snap-builder:test-base").returns(@mock_image)
+			Docker::Image.expects(:create).with('fromImage' => 'github-snap-builder:test-base').returns(@mock_image)
 
 			# Expect container based on image to be fired up
 			@mock_image.expects(:id).returns('1234')
@@ -101,16 +55,9 @@ module GithubSnapBuilder
 
 		private
 
-		def assert_image_created
-			# Now expect a new image to be built and tagged
-			dockerdir = File.join File.dirname(File.dirname(__dir__)), 'docker'
-			Docker::Image.expects(:build_from_dir).with(dockerdir, {
-				dockerfile: 'Dockerfile.test-base'
-			}).returns(@mock_image)
-			@mock_image.expects(:tag).with(repo: 'github-snap-builder', tag: 'test-base')
-		end
+		def assert_build(status_code)
+			Docker::Image.expects(:create).with('fromImage' => 'github-snap-builder:test-base').returns(@mock_image)
 
-		def assert_successful_build
 			# Expect container based on image to be fired up
 			@mock_image.expects(:id).returns('1234')
 			Docker::Container.expects(:create).with(
@@ -125,7 +72,7 @@ module GithubSnapBuilder
 			).returns(@mock_container)
 			@mock_container.expects(:start)
 			@mock_container.expects(:attach)
-			@mock_container.expects(:wait).returns({'StatusCode' => 0})
+			@mock_container.expects(:wait).returns({'StatusCode' => status_code})
 			@mock_container.expects(:delete).with(force: true)
 
 			builder = DockerBuilder.new('test-base')
