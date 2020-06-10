83fac19ce415172f371d09f8512696be115002fe
Kyle Fazzari <kyrofa@ubuntu.com>
2020-06-09 17:23:06 -0700

    Scaffold
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="github-snap-builder",
+    version="0.1.0",
+    author="Kyle Fazzari",
+    author_email="kyrofa@ubuntu.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    entry_points={"console_scripts": ["github-snap-builder=github_snap_builder.daemon:main"]},
+)
